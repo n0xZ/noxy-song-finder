2faef91fa7985b25021e1bb8b6a9d44c5a2cab90
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-03-02 16:44:55 -0300

    add public env variables on config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	modules: ['@unocss/nuxt'],
-	runtimeConfig: { public: { API_URL: process.env.API_URL } },
-	experimental: {
-		componentIslands: true,
+	runtimeConfig: {
+		public: {
+			API_URL: process.env.API_URL,
+			API_KEY: process.env.RAPID_API_KEY,
+			API_HOST: process.env.RAPID_API_HOST,
+		},
 	},
 	sourcemap: {
 		server: true,
