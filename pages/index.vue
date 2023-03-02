c2d42d469cc8630fee10f788336fe7feb624ae7d
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-03-02 16:51:01 -0300

    add homepage title

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,6 @@
-
+<script setup lang="ts">
+useHead({title:"Bienvenido a Noxy song finder!"})
+</script>
 <template>
    <div class="mt-12 h-full text-center flex flex-col justify-center space-y-2">
    <h3 class="text-2xl font-bold">Bienvenido a Song-finder!</h3>
