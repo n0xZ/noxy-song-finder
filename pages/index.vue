938493011a9f4d271cf4ca50d8eb204bf1ad2f31
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-03-02 16:48:13 -0300

    chore: setup homepage

---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+
+<template>
+   <div class="mt-12 h-full text-center flex flex-col justify-center space-y-2">
+   <h3 class="text-2xl font-bold">Bienvenido a Song-finder!</h3>
+   <p>Por favor, ingresa el nombre de la canción a buscar.</p>
+   </div> 
+</template>
