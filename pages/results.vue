04dd71874533e04a58aadeb8db91bf04c3768e2a
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-03-02 16:48:20 -0300

    chore: init results page

---
+++ pages/results.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+	useHead({ title: 'Noxy video finder - Resultados:' })
+	const {
+		query: { q },
+	} = useRoute()
+
+	const { data: search } = useResults(String(q))
+</script>
+
+<template>
+	<SearchResults :hits="search?.results" v-if="search?.results" />
+</template>
